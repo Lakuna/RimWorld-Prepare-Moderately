05e10418b23302b7dfd1484aa6014cccdfab1a05
Travis Martin <tjmartin2003@gmail.com>
2020-04-06 00:51:49 -0500

    Some work with Harmony patching. Thanks EdB.

--- Source/HarmonyPatches.cs
+++ Source/HarmonyPatches.cs
@@ -13,7 +13,33 @@ namespace PrepareModerately {
 	[StaticConstructorOnStartup]
 	class HarmonyPatches {
 		static HarmonyPatches() {
-			Harmony harmony = new Harmony("Lakuna.PrepareModerately");
+			try {
+				Harmony harmony = new Harmony("Lakuna.PrepareModerately");
+
+				Type configureStartingPawnsPageType = Type.GetType("RimWorld.Page_ConfigureStartingPawns");
+
+				if (configureStartingPawnsPageType == null) { Log.Warning("Failed to add elements to the configure pawns page (type not found)."); return; }
+
+				MethodBase preOpenMethod = configureStartingPawnsPageType.GetMethod("PreOpen");
+				HarmonyMethod preOpenMethodPostfix = new HarmonyMethod(typeof(HarmonyPatches).GetMethod("PreOpenPostfix"));
+
+				if (harmony.Patch(preOpenMethod, null, preOpenMethodPostfix) == null) { Log.Warning("Failed to patch the Page_ConfigureStartingPawns.PreOpen method."); }
+
+				MethodBase doWindowContentsMethod = configureStartingPawnsPageType.GetMethod("DoWindowContents");
+				HarmonyMethod doWindowContentsMethodPostfix = new HarmonyMethod(typeof(HarmonyPatches).GetMethod("DoWindowContentsPostfix"));
+
+				if (harmony.Patch(doWindowContentsMethod, null, doWindowContentsMethodPostfix) == null) { Log.Warning("Failed to patch the Page_ConfigureStartingPawns.DoWindowContentsPostfix method."); }
+			} catch (Exception e) { Log.Warning("Failed to patch for Prepare Moderately (unexpected exception).\n" + e.StackTrace); }
+		}
+
+		public static void PreOpenPostfix() {
+
+		}
+
+		public static void DoWindowContentsPostfix(Rect rect, Page_ConfigureStartingPawns instance) {
+			Vector2 position = new Vector2(150, 150);
+			float y = rect.height + 45;
+			if (!Widgets.ButtonText(new Rect((float) (rect.x + rect.width / 2.0 - position.x / 2.0), y, position.x, position.y), "Prepare Moderately")) { return; }
 		}
 	}
 }
