162aee8697762c164b02aac0ad78cb5383e1ad2b
Travis Martin <tjmartin2003@gmail.com>
2021-07-24 23:41:25 -0500

    Fix has any body modification filter.

--- Source/PrepareModerately/PawnFilter/PawnFilterParts/HasAnyBodyModification.cs
+++ Source/PrepareModerately/PawnFilter/PawnFilterParts/HasAnyBodyModification.cs
@@ -4,9 +4,7 @@ using Verse;
 
 namespace PrepareModerately.PawnFilter.PawnFilterParts {
 	public class HasAnyBodyModification : PawnFilterPart {
-		private static readonly ThingCategoryDef prostheticCategoryDef = DefDatabase<ThingCategoryDef>.GetNamed("BodyPartsProsthetic");
-
-		public override bool Matches(Pawn pawn) => pawn.health.hediffSet.hediffs.Any((hediff) => hediff.source.thingCategories.Contains(prostheticCategoryDef));
+		public override bool Matches(Pawn pawn) => pawn.def.race.body.AllParts.Any((bodyPart) => pawn.health.hediffSet.HasDirectlyAddedPartFor(bodyPart));
 
 		public override void DoEditInterface(Listing_PawnFilter listing) => listing.GetPawnFilterPartRect(this, 0);
 	}
