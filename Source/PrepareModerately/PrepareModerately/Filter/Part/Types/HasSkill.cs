4a95f313427c90a7308f3b647125d300ca8a5d4b
Travis Martin <tjmartin2003@gmail.com>
2025-06-13 21:16:11 -0500

    Refactor "has skill" to use a range.

--- Source/PrepareModerately/PrepareModerately/Filter/Part/Types/HasSkill.cs
+++ Source/PrepareModerately/PrepareModerately/Filter/Part/Types/HasSkill.cs
@@ -8,11 +8,16 @@ namespace Lakuna.PrepareModerately.Filter.Part.Types {
 	public class HasSkill : PawnFilterPart {
 		private SkillDef skill;
 
-		private int level;
+		private IntRange range;
 
-		public override bool Matches(Pawn pawn) => pawn == null
-			? throw new ArgumentNullException(nameof(pawn))
-			: pawn.skills.GetSkill(this.skill).levelInt >= this.level;
+		public override bool Matches(Pawn pawn) {
+			if (pawn == null) {
+				throw new ArgumentNullException(nameof(pawn));
+			}
+
+			int level = pawn.skills.GetSkill(this.skill).levelInt;
+			return level <= this.range.max && level >= this.range.min;
+		}
 
 		public override void DoEditInterface(PawnFilterEditListing listing, out float totalAddedListHeight) {
 			if (listing == null) {
@@ -30,26 +35,20 @@ namespace Lakuna.PrepareModerately.Filter.Part.Types {
 
 			float labelWidthPercentage = 0.2f;
 			Rect levelRect = new Rect(rect.x, skillRect.yMax, rect.width, Text.LineHeight);
-			Widgets.Label(levelRect.LeftPart(labelWidthPercentage).Rounded(), "LevelNumber".Translate(this.level).CapitalizeFirst());
-#if V1_0 || V1_1 || V1_2 || V1_3
-			this.level = (int)Widgets.HorizontalSlider(levelRect.RightPart(1 - labelWidthPercentage), this.level, 1, 20);
-#else
-			float level = this.level;
-			Widgets.HorizontalSlider(levelRect.RightPart(1 - labelWidthPercentage), ref level, new FloatRange(1, 20));
-			this.level = (int)level;
-#endif
+			Widgets.Label(levelRect.LeftPart(labelWidthPercentage).Rounded(), "Level".Translate().CapitalizeFirst());
+			Widgets.IntRange(levelRect.RightPart(1 - labelWidthPercentage), Rand.Int, ref this.range, 0, 20);
 		}
 
-		public override string Summary(PawnFilter filter) => "HasLevelInSkill".Translate(this.level, this.skill.label);
+		public override string Summary(PawnFilter filter) => "IsBetweenLevelsInSkill".Translate(this.range.min, this.range.max, this.skill.label);
 
 		public override void Randomize() {
 			this.skill = DefDatabase<SkillDef>.AllDefsListForReading.RandomElement();
-			this.level = Rand.Range(5, 10);
+			this.range = new IntRange(Rand.Range(0, 5), Rand.Range(5, 10));
 		}
 
 		public override void ExposeData() {
 			base.ExposeData();
-			Scribe_Values.Look(ref this.level, nameof(this.level));
+			Scribe_Values.Look(ref this.range, nameof(this.range));
 			Scribe_Defs.Look(ref this.skill, nameof(this.skill));
 		}
 	}
