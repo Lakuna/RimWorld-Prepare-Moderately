b5a020e8f9ebaffd607d9eec9df5f7fadcff2d32
Travis Martin <tjmartin2003@gmail.com>
2025-06-13 21:38:57 -0500

    Convert "has skills at level" to use ranges for skill count and level.

--- Source/PrepareModerately/PrepareModerately/Filter/Part/Types/HasSkillsAtLevel.cs
+++ Source/PrepareModerately/PrepareModerately/Filter/Part/Types/HasSkillsAtLevel.cs
@@ -6,13 +6,18 @@ using Verse;
 
 namespace Lakuna.PrepareModerately.Filter.Part.Types {
 	public class HasSkillsAtLevel : PawnFilterPart {
-		private int count;
+		private IntRange countRange;
 
-		private int level;
+		private IntRange range;
 
-		public override bool Matches(Pawn pawn) => pawn == null
-			? throw new ArgumentNullException(nameof(pawn))
-			: pawn.skills.skills.FindAll((skill) => skill.Level >= this.level).Count >= this.count;
+		public override bool Matches(Pawn pawn) {
+			if (pawn == null) {
+				throw new ArgumentNullException(nameof(pawn));
+			}
+
+			int count = pawn.skills.skills.FindAll((skill) => skill.Level <= this.range.max && skill.Level >= this.range.min).Count;
+			return count <= this.countRange.max && count >= this.countRange.min;
+		}
 
 		public override void DoEditInterface(PawnFilterEditListing listing, out float totalAddedListHeight) {
 			if (listing == null) {
@@ -23,37 +28,31 @@ namespace Lakuna.PrepareModerately.Filter.Part.Types {
 
 			float labelWidthPercentage = 0.2f;
 			Rect countRect = new Rect(rect.x, rect.y, rect.width, Text.LineHeight);
-			Widgets.Label(countRect.LeftPart(labelWidthPercentage).Rounded(), "CountNumber".Translate(this.count).CapitalizeFirst());
-#if V1_0 || V1_1 || V1_2 || V1_3
-			this.count = (int)Widgets.HorizontalSlider(countRect.RightPart(1 - labelWidthPercentage), this.count, 1, 12);
-#else
-			float count = this.count;
-			Widgets.HorizontalSlider(countRect.RightPart(1 - labelWidthPercentage), ref count, new FloatRange(1, 12));
-			this.count = (int)count;
-#endif
+			Widgets.Label(countRect.LeftPart(labelWidthPercentage).Rounded(), "Count".Translate().CapitalizeFirst());
+			Widgets.IntRange(countRect.RightPart(1 - labelWidthPercentage).Rounded(), Rand.Int, ref this.countRange, 0, 12);
 
 			Rect levelRect = new Rect(rect.x, countRect.yMax, rect.width, Text.LineHeight);
-			Widgets.Label(levelRect.LeftPart(labelWidthPercentage).Rounded(), "LevelNumber".Translate(this.level).CapitalizeFirst());
-#if V1_0 || V1_1 || V1_2 || V1_3
-			this.level = (int)Widgets.HorizontalSlider(levelRect.RightPart(1 - labelWidthPercentage), this.level, 1, 20);
-#else
-			float level = this.level;
-			Widgets.HorizontalSlider(levelRect.RightPart(1 - labelWidthPercentage), ref level, new FloatRange(1, 20));
-			this.level = (int)level;
-#endif
+			Widgets.Label(levelRect.LeftPart(labelWidthPercentage).Rounded(), "Level".Translate().CapitalizeFirst());
+			Widgets.IntRange(levelRect.RightPart(1 - labelWidthPercentage).Rounded(), Rand.Int, ref this.range, 0, 20);
 		}
 
-		public override string Summary(PawnFilter filter) => "HasSkillsAtLevel".Translate(this.count, this.level);
+		public override string Summary(PawnFilter filter) => this.countRange.min == this.countRange.max
+			? this.range.min == this.range.max
+				? "HasSkillsAtLevel".Translate(this.countRange.min, this.range.min)
+				: "HasSkillsBetweenLevels".Translate(this.countRange.min, this.range.min, this.range.max)
+			: this.range.min == this.range.max
+				? "HasBetweenSkillsAtLevel".Translate(this.countRange.min, this.countRange.max, this.range.min)
+				: "HasBetweenSkillsBetweenLevels".Translate(this.countRange.min, this.countRange.max, this.range.min, this.range.max);
 
 		public override void Randomize() {
-			this.count = Rand.Range(3, 6);
-			this.level = Rand.Range(5, 10);
+			this.countRange = new IntRange(Rand.Range(0, 1), Rand.Range(1, 3));
+			this.range = new IntRange(Rand.Range(0, 5), Rand.Range(5, 10));
 		}
 
 		public override void ExposeData() {
 			base.ExposeData();
-			Scribe_Values.Look(ref this.level, nameof(this.level));
-			Scribe_Values.Look(ref this.count, nameof(this.count));
+			Scribe_Values.Look(ref this.range, nameof(this.range));
+			Scribe_Values.Look(ref this.countRange, nameof(this.countRange));
 		}
 	}
 }
