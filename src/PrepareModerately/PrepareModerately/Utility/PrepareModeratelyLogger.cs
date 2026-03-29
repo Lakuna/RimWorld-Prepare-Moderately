674996436b8d9637f94a7afbc75b2fb82887bf88
Travis Martin <tjmartin2003@gmail.com>
2026-03-29 02:58:54 -0500

    Logger improvements from Bounded Rationality.

--- src/PrepareModerately/PrepareModerately/Utility/PrepareModeratelyLogger.cs
+++ src/PrepareModerately/PrepareModerately/Utility/PrepareModeratelyLogger.cs
@@ -3,14 +3,28 @@
 using Verse;
 
 namespace Lakuna.PrepareModerately.Utility {
-	public static class PrepareModeratelyLogger {
-		public static void LogException(Exception e, string description = "No description provided.",
-			PrepareModeratelyLoggerCategory category = PrepareModeratelyLoggerCategory.Unrestricted) {
-			if (e == null) {
+	/// <summary>
+	/// A static utility class that contains static utility methods for writing information to the log.
+	/// </summary>
+	internal static class PrepareModeratelyLogger {
+		/// <summary>
+		/// The prefix that is placed before all logged messages from this mod.
+		/// </summary>
+		private const string Prefix = "Prepare Moderately encountered an exception: ";
+
+		/// <summary>
+		/// Write information about an exception to the log.
+		/// </summary>
+		/// <param name="e">The exception.</param>
+		/// <param name="description">A description of the exception.</param>
+		/// <param name="category">The exception's category. If this isn't the default category, only one message of this category can be printed.</param>
+		/// <exception cref="ArgumentNullException">When no exception is given.</exception>
+		internal static void LogException(Exception e, string description = "No description provided.", PrepareModeratelyLoggerCategory category = PrepareModeratelyLoggerCategory.Unrestricted) {
+			if (e is null) {
 				throw new ArgumentNullException(nameof(e));
 			}
 
-			string output = $"Prepare Moderately encountered an exception: {description}\n";
+			string output = $"{Prefix}{description}\n";
 
 			Exception innerException = e;
 			while (innerException != null) {
@@ -22,20 +36,30 @@ namespace Lakuna.PrepareModerately.Utility {
 
 			if (category == PrepareModeratelyLoggerCategory.Unrestricted) {
 				Log.Error(output);
-			} else {
-				Log.ErrorOnce(output, (int)category);
+				return;
 			}
+
+			Log.ErrorOnce(output, (int)category);
 		}
 
-		public static void LogErrorMessage(string e, PrepareModeratelyLoggerCategory category = PrepareModeratelyLoggerCategory.Unrestricted) {
-			e = $"Prepare Moderately encountered an issue: {e}";
+		/// <summary>
+		/// Write information about an error with no corresponding exception to the log.
+		/// </summary>
+		/// <param name="e">The error message.</param>
+		/// <param name="category">The error's category. If this isn't the default category, only one message of this category can be printed.</param>
+		internal static void LogErrorMessage(string e, PrepareModeratelyLoggerCategory category = PrepareModeratelyLoggerCategory.Unrestricted) {
 			if (category == PrepareModeratelyLoggerCategory.Unrestricted) {
-				Log.Error(e);
-			} else {
-				Log.ErrorOnce(e, (int)category);
+				Log.Error(Prefix + e);
+				return;
 			}
+
+			Log.ErrorOnce(Prefix + e, (int)category);
 		}
 
-		public static void LogMessage(string message) => Log.Message(message);
+		/// <summary>
+		/// Write a non-error, non-exception message to the log.
+		/// </summary>
+		/// <param name="message">The message.</param>
+		internal static void LogMessage(string message) => Log.Message(message);
 	}
 }
